e52642c0e0135022a5df306bceb671415abdddbb
Ziad <zmali002@fiu.edu>
2021-09-26 09:20:54 -0400

    Database Preferences Updated

--- src/main/java/com/bsafe/bsafe/report/ReportConfig.java
+++ src/main/java/com/bsafe/bsafe/report/ReportConfig.java
@@ -12,12 +12,7 @@ public class ReportConfig {
     @Bean
     CommandLineRunner commandLineRunner(ReportRepository repository){
         return args -> {
-            Report report1 = new Report("TEST", "Reporting Abuse at my Home");
-            Report report2 = new Report("TEST", "Reporting Suspicious Activity at Neighbors House");
-            Report report3 = new Report("TEST", "Abuse between Couple at work Noticed");
-            repository.saveAll(
-                    List.of(report1, report2, report3)
-            );
+
         };
 
     }
