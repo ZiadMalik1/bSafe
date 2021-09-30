8462b06c5be19ff3518aa4e3486cffb7762dfda5
Ziad <zmali002@fiu.edu>
2021-09-30 18:21:10 -0400

    Server Changed

--- SpringAPI/src/main/java/com/bsafe/bsafe/BSafeApplication.java
+++ SpringAPI/src/main/java/com/bsafe/bsafe/BSafeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BSafeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BSafeApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(BSafeApplication.class, args);
     }
 
 }
