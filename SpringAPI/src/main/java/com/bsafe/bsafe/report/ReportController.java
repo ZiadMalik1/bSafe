dd3551d3e91d7cf5588f11fce644a4c8e00546e5
Ziad <zmali002@fiu.edu>
2021-09-30 20:28:32 -0400

    Server Changes

--- SpringAPI/src/main/java/com/bsafe/bsafe/report/ReportController.java
+++ SpringAPI/src/main/java/com/bsafe/bsafe/report/ReportController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(path = "api/v1/report")
 public class ReportController {
